23b5ec65870dfd9dfabf70b57ba8361ee76298f5
208417120 <208417120@gms.tku.edu.tw>
2023-03-22 19:27:12 +0800

    W06-P1: Deploy React to Vercel, show route /card2_node_xx

---
+++ demo/w05/1112-server-card-demo-20/routes/card2_20.js
@@ -0,0 +1,21 @@
+var express = require('express');
+var router = express.Router();
+
+const db = require('../utils/database');
+
+/* GET home page. */
+router.get('/', async function (req, res, next) {
+  try {
+    const results = await db.query(`select * from card_20`);
+    console.log('json data', JSON.stringify(results.rows));
+    res.render('card2_20/index', {
+      data: results.rows,
+      name: '吳昱霆',
+      id: '208417120',
+    });
+  } catch (error) {
+    console.log(error);
+  }
+});
+
+module.exports = router;
